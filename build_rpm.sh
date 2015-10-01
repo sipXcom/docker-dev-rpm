2daebd3b60cccb7eabc8e82de4cd8aff8b5335fb
dizzy <george@ezuce.com>
2015-10-01 17:21:17 +0300

    Change repo to unstable

--- build_rpm.sh
+++ build_rpm.sh
@@ -6,7 +6,7 @@ if [ "$1" = 'init' ]; then
     sudo rm -rf build
     sudo autoreconf -if
     sudo mkdir build && cd build
-    sudo ../configure --enable-rpm UPSTREAM_URL=http://download.sipxcom.org/pub/sipXecs/$SIPXCOM_VERSION/ CENTOS_RSYNC_URL=rsync://download.sipxcom.org/pub MIRROR_SITE=http://download.sipxcom.org/pub PACKAGE_REVISION=stable
+    sudo ../configure --enable-rpm UPSTREAM_URL=http://download.sipxcom.org/pub/$SIPXCOM_VERSION-unstable/ CENTOS_RSYNC_URL=rsync://download.sipxcom.org/pub MIRROR_SITE=http://download.sipxcom.org/pub PACKAGE_REVISION=stable
     sudo make repo-create
     sudo chown -R sipx:sipx /home/sipx/sipxcom/build/repo/
     sudo make repo-init
