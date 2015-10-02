041750ecb08731fd639c0c16da6d0eb35d84127d
dizzy <george@ezuce.com>
2015-10-02 08:59:19 +0300

    Setup build for all projects

--- build_rpm.sh
+++ build_rpm.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'init' ]; then
     cd /home/sipx/sipxcom
     sudo rm -rf build
     sudo autoreconf -if
-    sudo mkdir build && cd build
+    sudo mkdir build && cd build && sudo echo '$(sipx_all)' > .modules-include
     sudo ../configure --enable-rpm UPSTREAM_URL=http://download.sipxcom.org/pub/$SIPXCOM_VERSION-unstable/ CENTOS_RSYNC_URL=rsync://download.sipxcom.org/pub MIRROR_SITE=http://download.sipxcom.org/pub PACKAGE_REVISION=stable
     sudo make repo-create
     sudo chown -R sipx:sipx /home/sipx/sipxcom/build/repo/
